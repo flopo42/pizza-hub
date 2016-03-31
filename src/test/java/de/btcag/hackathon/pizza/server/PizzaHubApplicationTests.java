95004cd64df588b2d9811ca0ba555548cc429b27
Florian Postel <florian.postel@btc-ag.com>
2016-03-31 21:54:15 +0200

    spring initializr

---
+++ src/test/java/de/btcag/hackathon/pizza/server/PizzaHubApplicationTests.java
@@ -0,0 +1,16 @@
+package de.btcag.hackathon.pizza.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PizzaHubApplication.class)
+public class PizzaHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
